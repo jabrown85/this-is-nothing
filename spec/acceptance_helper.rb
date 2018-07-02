a25602b6d562eb9a2f8ceaf2ee1fcb970532f514
Jesse Brown <jabrown85@gmail.com>
2018-07-02 08:54:21 -0500

    disable extensions

--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -9,10 +9,11 @@ require 'selenium/webdriver'
 options = Selenium::WebDriver::Chrome::Options.new
 chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
 options.binary = chrome_bin if chrome_bin
-#options.add_argument('--headless')
+options.add_argument('--headless')
 options.add_argument('--window-size=1875,1275')
-#options.add_argument('--no-sandbox')
-#options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-extensions')
 
 driver = Selenium::WebDriver.for :chrome, options: options
 driver.navigate.to "http://google.com"
