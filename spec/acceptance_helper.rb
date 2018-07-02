5a021f4018ebd5167ab0242c5ed61c3fdef024d0
Jesse Brown <jabrown85@gmail.com>
2018-07-02 09:18:20 -0500

    port?

--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -9,12 +9,13 @@ require 'selenium/webdriver'
 options = Selenium::WebDriver::Chrome::Options.new
 chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
 options.binary = chrome_bin if chrome_bin
-options.add_argument('no-sandbox')
-options.add_argument('disable-dev-shm-usage')
-options.add_argument('headless')
-options.add_argument('disable-gpu')
-options.add_argument('window-size=1875,1275')
-options.add_argument('disable-extensions')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--window-size=1875,1275')
+options.add_argument('--disable-extensions')
+options.add_argument('--remote-debugin-port=9222')
 
 driver = Selenium::WebDriver.for :chrome, options: options
 driver.navigate.to "http://google.com"
