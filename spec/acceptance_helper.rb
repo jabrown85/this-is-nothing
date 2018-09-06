8aad9c74e272c06af0287a626703e69f76e667c3
Jesse Brown <jabrown85@gmail.com>
2018-09-05 19:14:22 -0500

    more testing

--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -23,7 +23,7 @@ options.add_argument('disable-dev-shm-usage')
 options.add_argument('window-size=1400,1400')
 
 driver = Selenium::WebDriver.for :chrome, options: options
-driver.navigate.to "http://google.com"
+#driver.navigate.to "http://google.com"
 
 
 require 'capybara'
@@ -32,17 +32,17 @@ require 'capybara/rspec'
 
 Capybara.app = App
 
-# chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
-# chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
 
-# Capybara.register_driver :chrome do |app|
-#   Capybara::Selenium::Driver.new(
-#      app,
-#      browser: :chrome,
-#      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
-#   )
-# end
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(
+     app,
+     browser: :chrome,
+     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
+  )
+end
 
-# RSpec.configure do |config|
-#   config.include Capybara
-# end
+RSpec.configure do |config|
+  config.include Capybara
+end
--- spec/browser_spec.rb
+++ spec/browser_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/acceptance_helper'
 
 feature 'Navigation' do
   scenario 'they visit the hello page' do
-    visit 'https://google.com'
-    expect(page).to have_content 'About'
+    visit '/'
+    expect(page).to have_content 'testing'
   end
 end
