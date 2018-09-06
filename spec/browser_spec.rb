216b53879f2ce271a17162ef582988766e142e49
Jesse Brown <jabrown85@gmail.com>
2018-09-05 19:07:55 -0500

    trying to hit google instead

--- spec/browser_spec.rb
+++ spec/browser_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/acceptance_helper'
 
 feature 'Navigation' do
   scenario 'they visit the hello page' do
-    visit '/'
-    expect(page).to have_content 'testing'
+    visit 'https://google.com'
+    expect(page).to have_content 'About'
   end
 end
